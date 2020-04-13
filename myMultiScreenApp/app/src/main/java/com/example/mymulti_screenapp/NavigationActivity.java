3da052c600331ec94915aaa30376a5ad2572abe1
admin <admin@macbook-pro-69.home>
2020-04-12 21:58:56 -0400

    Completed the navigation page. I need to fill in each of the pages now

--- myMultiScreenApp/app/src/main/java/com/example/mymulti_screenapp/NavigationActivity.java
+++ myMultiScreenApp/app/src/main/java/com/example/mymulti_screenapp/NavigationActivity.java
@@ -14,14 +14,6 @@ public class NavigationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
 
-        ArrayList<NavigationItem> navigationItems = new ArrayList<NavigationItem>();
-        navigationItems.add(new NavigationItem("About COVID-19","Go",R.drawable.ic_launcher_background));
-        navigationItems.add(new NavigationItem("Protect Yourself","Go",R.drawable.ic_launcher_background));
-        navigationItems.add(new NavigationItem("Statistics","Go",R.drawable.ic_launcher_background));
-        navigationItems.add(new NavigationItem("Play Game","Go",R.drawable.ic_launcher_background));
 
-        ListView navigationListView = findViewById(R.id.navigationListView);
-        NavigationAdapter navigationAdapter = new NavigationAdapter(this, navigationItems);
-        navigationListView.setAdapter(navigationAdapter);
     }
 }
